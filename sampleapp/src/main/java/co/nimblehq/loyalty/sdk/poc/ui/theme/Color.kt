11edf73af40de39542cc4c3ae04f7318d5ec2a14
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-03-28 13:39:00 +0700

    [Chore] Extract colors variable

--- sampleapp/src/main/java/co/nimblehq/loyalty/sdk/poc/ui/theme/Color.kt
+++ sampleapp/src/main/java/co/nimblehq/loyalty/sdk/poc/ui/theme/Color.kt
@@ -11,3 +11,7 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val Indigo = Color(0xFF303F9F)
+
+val White = Color(0xFFFFFFFF)
+val LightGrey = Color(0xFFEDF2F7)
+val Blue = Color(0xFF5A67D8)
